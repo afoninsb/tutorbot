7840b92bf805b5d36a7dfc1842c8bf44d514f4c6
Sergey Afonin <afoninsb@yandex.ru>
2022-12-11 15:51:52 +0300

    1

--- tutor_bot/regbot/apps.py
+++ tutor_bot/regbot/apps.py
@@ -6,22 +6,22 @@ class RegbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'regbot'
 
-    def ready(self):
-        from edubot.main_classes import BotData
-        bot = BotData(settings.REGBOT_TOKEN)
-        if settings.DEBUG:
-            data = {
-                'url': f'https://{settings.NGROK}/webhook/reg/{settings.REGBOT_TOKEN}/'
-            }
-        else:
-            data = {
-                'url': f'{settings.BASE_URL}/webhook/reg/{settings.REGBOT_TOKEN}/'
-            }
-        bot.set_webhook(data)
-        bot.commands = [
-            {
-                'command': 'cancel',
-                'description': 'Отменить текущую операцию'
-            },
-        ]
-        bot.set_commands()
+    # def ready(self):
+    #     from edubot.main_classes import BotData
+    #     bot = BotData(settings.REGBOT_TOKEN)
+    #     if settings.DEBUG:
+    #         data = {
+    #             'url': f'https://{settings.NGROK}/webhook/reg/{settings.REGBOT_TOKEN}/'
+    #         }
+    #     else:
+    #         data = {
+    #             'url': f'{settings.BASE_URL}/webhook/reg/{settings.REGBOT_TOKEN}/'
+    #         }
+    #     bot.set_webhook(data)
+    #     bot.commands = [
+    #         {
+    #             'command': 'cancel',
+    #             'description': 'Отменить текущую операцию'
+    #         },
+    #     ]
+    #     bot.set_commands()
