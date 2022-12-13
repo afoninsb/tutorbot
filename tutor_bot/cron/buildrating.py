a2af6e79dc476e67211d9cab7d611210dc5fc0ea
/usr/bin/vim <support@beget.ru>
2022-12-13 12:46:44 +0300

    fix rating

--- tutor_bot/cron/buildrating.py
+++ tutor_bot/cron/buildrating.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from bots.models import Bot
 from content.models import Log
@@ -6,36 +6,35 @@ from stats.models import Rating
 
 
 def rating(tokens: list, now):
-    yesterday = (now - timedelta(days=1)).strftime('%Y-%m-%d')
-    now = now.strftime('%Y-%m-%d')
-    for token in tokens:
-        bot = Bot.objects.get(token=token)
-        objs = []
-
-        from_log = Log.objects.\
-            filter(bot=bot).\
-            filter(time__gte=now).\
-            filter(is_truth=True).\
-            select_related('student')
-        log_scores = {log.student: log.score for log in from_log}
-        from_rating = Rating.objects.\
-            filter(bot=bot).\
-            filter(time=yesterday).\
-            select_related('student')
-        for rating in from_rating:
-            scores = rating.score
-            if rating.student in log_scores:
-                scores += log_scores[rating.student]
-                del log_scores[rating.student]
-            objs.append(
-                Rating(
-                    bot=bot,
-                    student=rating.student,
-                    score=scores
+        yesterday = (now - timedelta(days=1))
+        yesterday = datetime(yesterday.year, yesterday.month, yesterday.day)
+        now = datetime(now.year, now.month, now.day)
+        for token in tokens:
+            bot = Bot.objects.get(token=token)
+            objs = []
+            from_log = Log.objects.\
+                filter(bot=bot).\
+                filter(time__gte=now).\
+                select_related('student')
+            log_scores = {log.student: log.score for log in from_log}
+            from_rating = Rating.objects.\
+                filter(bot=bot).\
+                filter(time=yesterday).\
+                select_related('student')
+            for rating in from_rating:
+                scores = rating.score
+                if rating.student in log_scores:
+                    scores += log_scores[rating.student]
+                    del log_scores[rating.student]
+                objs.append(
+                    Rating(
+                        bot=bot,
+                        student=rating.student,
+                        score=scores
+                    )
                 )
-            )
-        if log_scores:
-            objs.extend(Rating(bot=bot, student=student, score=score)
-                        for student, score in log_scores.items())
-
-        Rating.objects.bulk_create(objs)
+            if log_scores:
+                objs.extend(Rating(bot=bot, student=student, score=score)
+                            for student, score in log_scores.items())
+    
+            Rating.objects.bulk_create(objs)
