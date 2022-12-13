890e2601c26e036729444d6244bfb431d40676be
/usr/bin/vim <support@beget.ru>
2022-12-13 12:47:15 +0300

    fix rating

--- tutor_bot/cron/cron.py
+++ tutor_bot/cron/cron.py
@@ -20,7 +20,7 @@ def cron_task():
             and hour in bot_hours
         ):
             tokens_tasks.append(bot.token)
-        if hour == '12':
+        if hour == '3':
             tokens_rating.append(bot.token)
 
     if tokens_tasks:
