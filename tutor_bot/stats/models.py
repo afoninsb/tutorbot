2723ad593a75b45772e96b795caf12d411ab54cc
Sergey Afonin <afoninsb@yandex.ru>
2022-12-15 20:41:03 +0300

    menu in readme

--- tutor_bot/stats/models.py
+++ tutor_bot/stats/models.py
@@ -28,3 +28,24 @@ class Rating(models.Model):
         verbose_name = 'Рейтинг'
         verbose_name_plural = 'Рейтинги'
         ordering = ('-score',)
+
+
+class Alert(models.Model):
+    """Модель алертов."""
+
+    class AlertsTypes(models.IntegerChoices):
+        NEWUSER = 1, 'Новый ученик.'
+        FEWTASKS = 2, 'В категории мало задач.'
+        TASKSAREOVER = 3, 'В категории кончились задачи. Она отключена.'
+        DISABLEBOT = 4, 'В боте кончились задачи. Он отключен.'
+
+    difficulty = models.PositiveSmallIntegerField(
+        verbose_name='Типы алертов',
+        choices=AlertsTypes.choices,
+    )
+    time = models.DateTimeField(
+        verbose_name='Дата алерта',
+    )
+    attribute = models.PositiveIntegerField(
+        verbose_name='ID объекта алерта',
+    )
