f88ddae70bf3bd4be5b045a4993238616fb5a3af
Sergey Afonin <afoninsb@yandex.ru>
2023-02-14 10:34:48 +0300

    reting

--- tutor_bot/cron/rating.py
+++ tutor_bot/cron/rating.py
@@ -14,7 +14,7 @@ def cron_rating():
     for bot in bots:
         now = datetime.now(pytz.timezone(bot.tz))
         today = datetime(now.year, now.month, now.day)
-        if bot.last_rating < today:
+        if str(bot.last_rating) < str(today):
         # hour = str(now.hour)
         # if hour in {'2', '8', '14', 20', '02', '08'}:
             tokens_rating.append((bot.token, today))
