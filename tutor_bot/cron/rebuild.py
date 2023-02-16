a217ecca35a5cf75ec5f9ae65ca705c0a2edc7fd
Sergey Afonin <afoninsb@yandex.ru>
2023-02-16 09:48:51 +0300

    1

--- tutor_bot/cron/rebuild.py
+++ tutor_bot/cron/rebuild.py
@@ -9,7 +9,7 @@ def rating(tokens):
     for token in tokens:
         bot = Bot.objects.get(id=token)
         for plus_day in range(68):
-            today = (datetime(2023, 1, 13) + timedelta(days=plus_day))
+            today = (datetime(2022, 12, 11) + timedelta(days=plus_day))
             yesterday = (today - timedelta(days=1))
             objs = []
             from_log = Log.objects.\
